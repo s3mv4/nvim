0df202f5704ca627e766a503140c6636f293d019
s3m <s3m@archlinux.org>
2022-10-05 11:58:41 +0200

    added server support

--- lua/s3m/lsp/lsp-installer.lua
+++ lua/s3m/lsp/lsp-installer.lua
@@ -16,6 +16,16 @@ lsp_installer.on_server_ready(function(server)
         opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
     end
 
+    if server.name == "pyright" then
+        local pyright_opts = require("s3m.lsp.settings.pyright")
+        opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+    end
+
+    if server.name == "jsonls" then
+        local jsonls_opts = require("s3m.lsp.settings.jsonls")
+        opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+    end
+
     -- This setup() function is exactly the same as lspconfig's setup function.
     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     server:setup(opts)
