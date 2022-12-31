322b82648db55dbbd80070cf5b9bc6151c4c143e
s3m <s3m@archlinux.org>
2022-12-31 16:14:11 +0100

    added autopairs

--- lua/s3m/treesitter.lua
+++ lua/s3m/treesitter.lua
@@ -1,8 +1,15 @@
-local configs = require("nvim-treesitter.configs")
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+    return
+end
+
 configs.setup {
     ensure_installed = "all",
     sync_install = false, 
     ignore_install = { "" }, -- List of parsers to ignore installing
+    autopairs = {
+        enable = true,
+    },
     highlight = {
         enable = true, -- false will disable the whole extension
         disable = { "" }, -- list of language that will be disabled
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -119,6 +119,11 @@ _G.packer_plugins = {
     path = "/home/sem/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
     url = "https://github.com/ellisonleao/gruvbox.nvim"
   },
+  ["nvim-autopairs"] = {
+    loaded = true,
+    path = "/home/sem/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    url = "https://github.com/windwp/nvim-autopairs"
+  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "/home/sem/.local/share/nvim/site/pack/packer/start/nvim-cmp",
